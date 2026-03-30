554c5a43a88153024b7b31fd18ed2431621b1986
qwerty吃小庄 <git@qwertycxz.top>
2026-03-30 17:16:42 +0800

    chore: Update Java compatibility to 8
    for Minecraft 1.16.5 to 1.21.11

--- src/main/java/top/qwertycxz/metadatawildcard/MetadataWildcard.java
+++ src/main/java/top/qwertycxz/metadatawildcard/MetadataWildcard.java
@@ -1,15 +1,16 @@
 package top.qwertycxz.metadatawildcard;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.Files.createDirectories;
 import static java.nio.file.Files.lines;
-import static java.nio.file.Files.writeString;
+import static java.nio.file.Files.write;
 import static java.util.Optional.empty;
 import static java.util.concurrent.CompletableFuture.completedFuture;
 import static java.util.stream.Collectors.toSet;
 import static me.lucko.fabric.api.permissions.v0.Options.get;
 import static net.fabricmc.fabric.api.event.Event.DEFAULT_PHASE;
 import static net.fabricmc.loader.api.FabricLoader.getInstance;
-import static net.minecraft.resources.Identifier.tryParse;
+import static net.minecraft.resources.ResourceLocation.tryParse;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -17,7 +18,7 @@ import java.util.concurrent.CopyOnWriteArraySet;
 import me.lucko.fabric.api.permissions.v0.OfflineOptionRequestEvent;
 import me.lucko.fabric.api.permissions.v0.OptionRequestEvent;
 import net.fabricmc.api.DedicatedServerModInitializer;
-import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceLocation;
 
 /// An addon for fabric-permissions-api that enables wildcard support in metadata.
 /// # Example
@@ -34,7 +35,7 @@ public class MetadataWildcard implements DedicatedServerModInitializer {
 	/// Only look up metadata with these prefixes.
 	public static final CopyOnWriteArraySet<String> prefixStrings = new CopyOnWriteArraySet<String>();
 	/// A phase after default phase.
-	private static final Identifier WILDCARD_PHASE = tryParse("$lowercase");
+	private static final ResourceLocation WILDCARD_PHASE = tryParse("$lowercase");
 	/// `foo.bar.baz.qux` -> `qux`
 	///
 	/// `foo.bar.baz.*` -> `baz.*`
@@ -71,9 +72,7 @@ public class MetadataWildcard implements DedicatedServerModInitializer {
 			}
 			catch (IOException e) {
 				createDirectories(configDirectory);
-				writeString(prefixConfig, """
-				minecraft.selector
-				""");
+				write(prefixConfig, "minecraft.selector\n".getBytes(UTF_8));
 			}
 			prefixStrings.addAll(lines(prefixConfig).parallel().collect(toSet()));
 		}
